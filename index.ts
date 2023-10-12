edbc45fe22e22704eeb05790b6d667b3d20e4e67
Saradhi <saradhi0001069@gmail.com>
2023-10-12 18:35:28 +0530

    added mongo db connection

--- index.ts
+++ index.ts
@@ -1,15 +1,22 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import {connection} from "./db/mongodbConnection";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
-
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
+app.listen(port, async() => {
+  try{
+    await connection;
+    console.log(`mongodb is connected &`);
+  }catch(err)
+  {
+    console.log(err);
+  }
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
