48059c372e3751358b3002d24b69e3f0ec0f2d7f
Saradhi <saradhi0001069@gmail.com>
2023-10-17 15:36:42 +0530

    added rebase signup

--- controllers/signup/userSignUpController.ts
+++ controllers/signup/userSignUpController.ts
@@ -1,32 +1,26 @@
 import { Request, Response } from 'express';
 import SimulatorUserModel, { SimulatorUser } from '../../models/userModel';
+import bcrypt from "bcrypt";
 
 export const userSignUp = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-
-  // Validate email and password (you can add more validation logic as needed)
-  if (!email || !password) {
-    return res.status(400).json({ status: 'failure', error: 'Email and password are required.' });
-  }
-
   try {
-    // Check if the user exists with the provided email and password
-    console.log(email, password);
-    const user: SimulatorUser | null = await SimulatorUserModel.findOne({
+    const { email, password } = req.body;
+
+    // Hash the password before storing it (you can use bcrypt)
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user instance
+    const newUser: SimulatorUser = new SimulatorUserModel({
       email,
-      password,
+      password: hashedPassword,  // Store the hashed password
     });
-    console.log("user",user);
-    if (user) {
-      // User found, you can send a success response or perform further actions
-      console.log('User email:', email);
-      return res.status(200).json({ status: 'success', message: 'Login successful' });
-    } else {
-      // User not found
-      return res.status(404).json({ status: 'failure', error: 'User not found' });
-    }
+
+    // Save the user to the database
+    await newUser.save();
+
+    res.status(201).send('User registered successfully.');
   } catch (error) {
-    console.error('Error during login:', error);
-    return res.status(500).json({ status: 'failure', error: 'Internal server error' });
+    console.error('Error during user registration:', error);
+    res.status(500).send('An error occurred during registration.');
   }
 };
--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { userLogin } from "../controllers/login/userLoginController";
+import { userSignUp } from "../controllers/signup/userSignUpController";
 const userRoute = express.Router();
-
 userRoute.post("/userlogin",userLogin);
+userRoute.post("/usersignup",userSignUp);
 export default userRoute;
