a0423e41f950681aabff4f9e5793f59c7f790bfb
Saradhi <99667252+Saradhii@users.noreply.github.com>
2024-01-11 12:18:10 +0530

    Update userSignUpController.ts

--- controllers/signup/userSignUpController.ts
+++ controllers/signup/userSignUpController.ts
@@ -5,16 +5,13 @@ import bcrypt from "bcrypt";
 export const userSignUp = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
-
     // Hash the password before storing it (you can use bcrypt)
     const hashedPassword = await bcrypt.hash(password, 10);
-
     // Create a new user instance
     const newUser: SimulatorUser = new SimulatorUserModel({
       email,
       password: hashedPassword,  // Store the hashed password
     });
-
     // Save the user to the database
     await newUser.save();
     return res.status(200).json({ status: 'success', message: 'User registered successfully.' });
