0f5fcb2936d4d33302c87a2417286ec457129099
Saradhi <99667252+Saradhii@users.noreply.github.com>
2024-02-03 23:43:43 +0530

    Update userRoutes.ts

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -4,7 +4,6 @@ import { userSignUp } from "../controllers/signup/userSignUpController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 import { userDetails } from "../controllers/userDetails/userDetails";
 const userRoute = express.Router();
-
 userRoute.post("/userlogin",userLogin);
 userRoute.post("/usersignup",userSignUp);
 userRoute.get("/userdetails",authMiddleware,userDetails);
